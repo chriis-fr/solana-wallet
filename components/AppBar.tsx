62814c7b5613d2252a4631777438a9131fe14a07
christopher odhiambo <caspianodhis@gmail.com>
2024-05-08 23:35:55 +0300

    addition to wallet

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
             <Image src="/solanaLogo.png" height={30} width={200} alt='solana'/>
             <span>Wallet-Adapter Test</span>
-            <button>Connect</button>
+            <WalletMultiButton />
         </div>
     )
 }
